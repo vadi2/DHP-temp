55706db87c5e92cea10b181107d22adb11d4fe80
Vadim Peretokin <vperetokin@hey.com>
2025-08-07 10:09:59 +0200

    Add credit for script

--- updateSiteBasedOnMarkdownChange.sh
+++ updateSiteBasedOnMarkdownChange.sh
@@ -1,3 +1,5 @@
+# credit to Bas van den Heuvel, original script from https://github.com/HL7/fhircast-docs/blob/master/updateSiteBasedOnMarkdownChange.sh
+
 cd temp/pages
 cp ../../input/pagecontent/* _includes/; jekyll b
 
